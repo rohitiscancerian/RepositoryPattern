d6dc4624586b24e1865a93900d577c0aff6aa7c1
rohitiscancerian <er.rohitkumar@gmail.com>
2015-12-21 22:28:19 +0000

    Adding the files

---
+++ Interview/CarSqlDataStoreStrategy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Interview
+{
+    public class CarSqlDataStoreStrategy :  SqlDataStoreStrategy<Car> , IDataStoreStrategy<Car>
+    {
+
+        public CarSqlDataStoreStrategy(IDataStoreContext dataStoreContext) : base (dataStoreContext)
+        { }
+
+        public CarSqlDataStoreStrategy() : base ()
+        {
+
+        }
+    }
+}
